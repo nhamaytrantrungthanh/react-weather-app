c13fac76df6e10dce8f92946fd05b6b58c8570ce
Tran Thanh <tranthanh@MacBook-Air-cua-Tran.local>
2023-06-06 10:17:52 +0700

    weather

---
+++ src/components/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "ce401bc80bmshab34d80334c931ep12c842jsnc14b8abe1dc6",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "c07f2fc597bf033376ee086e7b12fa02";
